da9c9187c8b98ee3f704e1a1756993290cc00e0d
Gerihh <horvathgergo1208@gmail.com>
2024-01-18 20:36:52 +0100

    register auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        //Validálás
+        $data = $request->all();
+
+        $rules = [
+            'username' =>'required|unique:users|min:6',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8|regex:/^(?=.*[A-Z])(?=.*[0-9])/'
+        ];
+        $messages = [
+            'username.required' => 'Felhasználónév megadása kötelező',
+            'username.unique' => 'A megadott felhasználónév már foglalt',
+            'username.min' => 'A felhasználónév legalább 6 karakter hosszú kell legyen',
+            'email.required' => 'E-mail cím megadása kötelező',
+            'email.email' => 'Érvénytelen e-mail cím',
+            'email.unique' => 'A megadott e-mail cím már foglalt',
+            'password.required' => 'Jelszó megadása kötelező',
+            'password.string' => 'A jelszónak karaktereket kell tartalmaznia',
+            'password.min' => 'A jelszónak legalább 8 karakterből kell állnia',
+            'password.regex' => 'A jelszónak legalább egy nagybetűt és egy számot kell tartalmaznia'
+        ];
+
+
+        $validator = Validator::make($data, $rules, $messages);
+
+        //Validálás check
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response ()->json($errors, 400);
+        } else { //User létrehozása
+           $user = User::create([
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+           ]);
+    }
+        //Token kreálás
+        $token = $user->createToken('authToken')->accessToken;
+
+        return response()->json([
+            'user' => $user,
+            'accessToken' => $token,
+        ]);
+    }
+    public function login(Request $request)
+    {
+
+    }
+    public function logout()
+    {
+        Auth::logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+    public function refresh()
+    {
+
+    }
+    public function user()
+    {
+        $user = Auth::user();
+
+        return response()->json($user);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,5 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::get('user', [AuthController::class, 'user']);
+});
 
 Route::apiResource('users', UserController::class);
