c24b02b9bc816d4812beee6dfdf74f0668687869
Gerihh <horvathgergo1208@gmail.com>
2024-01-26 11:32:16 +0100

    uploadController

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,19 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
     public function upload(Request $request)
     {
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
-            $image->move(public_path('uploads'), $imageName);
+        try {
+            if ($request->hasFile('image')) {
+                $image = $request->file('image');
+                $imageName = time() . '.' . $image->getClientOriginalExtension();
+                $image->storeAs('uploads', $imageName, 'public');
 
-            return response()->json(['imageUrl' => '/uploads/' . $imageName], 200);
+                $imageUrl = '/uploads/' . $imageName;
+
+                return response()->json(['imageUrl' => $imageUrl], 200);
+            } else {
+                return response()->json(['message' => 'No file provided.'], 400);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error uploading image.', 'error' => $e->getMessage()], 500);
         }
-
-        return response()->json(['message' => 'No file provided.'], 400);
     }
 }
